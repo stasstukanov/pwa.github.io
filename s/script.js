8036084ce014691fab17a4f1e498be11003bb9e6
Stas Stukanov <50484376+stasstukanov@users.noreply.github.com>
2021-05-18 22:26:50 +0300

    update

--- s/script.js
+++ s/script.js
@@ -3,3 +3,13 @@ function myFunction() {
     x.style.fontSize = "25px"; 
     x.style.color = "red"; 
   }
+
+        if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  navigator.serviceWorker.register('sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
