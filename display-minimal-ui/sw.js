0c1cfac24e14a07d1bfaae675d449ba0d73ad07d
Stas Stukanov <50484376+stasstukanov@users.noreply.github.com>
2021-06-11 15:00:28 +0300

    Update sw.js

--- display-minimal-ui/sw.js
+++ display-minimal-ui/sw.js
@@ -30,12 +30,4 @@ self.addEventListener('install', function(event) {
       })
     );
   })
-  window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('👍', 'beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
-    window.deferredPrompt = event;
-    // Remove the 'hidden' class from the install button container
-    divInstall.classList.toggle('hidden', false);
-  });
-
   
