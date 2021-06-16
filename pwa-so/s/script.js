ee950286167f76da02166948ee8236f658580f90
Stas Stukanov <50484376+stasstukanov@users.noreply.github.com>
2021-06-16 21:52:59 +0300

    pwa offers

---
+++ pwa-so/s/script.js
@@ -0,0 +1,17 @@
+/*
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px"; 
+    x.style.color = "red"; 
+  }
+
+        if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  navigator.serviceWorker.register('sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+*/
